6dc3870fde3158e57afbdd1975dde4951bdc7455
cvigueras <carlosj.vigueras@gmail.com>
2023-04-17 12:35:56 +0200

    Test: GetEmptyBoardWhenRoundStart

--- TicTacToe.Test/BoardShould.cs
+++ TicTacToe.Test/BoardShould.cs
@@ -1,4 +1,6 @@
-﻿namespace TicTacToe.Test
+﻿using FluentAssertions;
+
+namespace TicTacToe.Test
 {
     public class BoardShould
     {
@@ -6,6 +8,22 @@
         public void Setup()
         {
 
+        }
+
+        [Test]
+        public void GetEmptyBoardWhenRoundStart()
+        {
+            var result = Board.Print();
+
+            result.Should().Be("[ ][ ][ ][ ][ ][ ][ ][ ][ ]");
+        }
+    }
+
+    public class Board
+    {
+        public static object Print()
+        {
+            throw new NotImplementedException();
         }
     }
 }
