95e8aae7e3f574a166d327b4859b368c33c07a01
cvigueras <carlosj.vigueras@gmail.com>
2023-04-18 11:06:04 +0200

    Refactor: add logic to get winner in columns

--- TicTacToe.Console/Board.cs
+++ TicTacToe.Console/Board.cs
@@ -28,24 +28,17 @@ public class Board
 
     private string CheckWinnerByColumn()
     {
-        if (_value[0, 0].Contains("X") && _value[1, 0].Contains("X") && _value[2, 0].Contains("X"))
+        if (_value[0, 0] != "[ ]" && _value[0, 0] == _value[1, 0] && _value[0, 0] == _value[2, 0])
         {
-            return "Player X WIN!";
+            return $"Player {_value[0, 0].Replace("[", "").Replace("]", "")} WIN!";
         }
-
-        if (_value[0, 0].Contains("Y") && _value[1, 0].Contains("Y") && _value[2, 0].Contains("Y"))
+        if (_value[0, 1] != "[ ]" && _value[0, 1] == _value[1, 1] && _value[0, 1] == _value[2, 1])
         {
-            return "Player Y WIN!";
+            return $"Player {_value[0, 1].Replace("[", "").Replace("]", "")} WIN!";
         }
-
-        if (_value[0, 1].Contains("X") && _value[1, 1].Contains("X") && _value[2, 1].Contains("X"))
+        if (_value[0, 2] != "[ ]" && _value[0, 2] == _value[1, 2] && _value[0, 2] == _value[2, 2])
         {
-            return "Player X WIN!";
-        }
-
-        if (_value[0, 1].Contains("Y") && _value[1, 1].Contains("Y") && _value[2, 1].Contains("Y"))
-        {
-            return "Player Y WIN!";
+            return $"Player {_value[0, 2].Replace("[", "").Replace("]", "")} WIN!";
         }
 
         return string.Empty;
