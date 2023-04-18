14a7c644485733445b186a963d2588c6a8401d64
cvigueras <carlosj.vigueras@gmail.com>
2023-04-18 10:48:10 +0200

    Test: green WinPlayerXByThirdRowRow

--- TicTacToe.Test/BoardShould.cs
+++ TicTacToe.Test/BoardShould.cs
@@ -142,5 +142,19 @@ namespace TicTacToe.Test
 
             result.Should().Be("Player Y WIN!");
         }
+
+        [Test]
+        public void WinPlayerXByThirdRowRow()
+        {
+            _board.AddMotion(Token.X, new Position(2, 0));
+            _board.AddMotion(Token.Y, new Position(0, 0));
+            _board.AddMotion(Token.X, new Position(2, 1));
+            _board.AddMotion(Token.Y, new Position(1, 0));
+            _board.AddMotion(Token.X, new Position(2, 2));
+
+            var result = _board.Print();
+
+            result.Should().Be("Player X WIN!");
+        }
     }
 }
