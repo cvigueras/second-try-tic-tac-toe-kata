61ed1fe7fa052fe8e99fc6a1a408c25ae63310f2
cvigueras <carlosj.vigueras@gmail.com>
2023-04-18 09:07:57 +0200

    Refactor: extract method in production code.

--- TicTacToe.Console/Board.cs
+++ TicTacToe.Console/Board.cs
@@ -11,19 +11,32 @@ public class Board
 
     public string Print()
     {
+        var winner = CheckWinnerByRow();
+        if (!string.IsNullOrEmpty(winner))
+        {
+            return winner;
+        }
+        return _value.Cast<string?>().Aggregate(string.Empty, (current, item) => current + item);
+    }
+
+    private string CheckWinnerByRow()
+    {
         if (_value[0, 0].Contains("X") && _value[0, 1].Contains("X") && _value[0, 2].Contains("X"))
         {
             return "Player X WIN!";
         }
+
         if (_value[0, 0].Contains("Y") && _value[0, 1].Contains("Y") && _value[0, 2].Contains("Y"))
         {
             return "Player Y WIN!";
         }
+
         if (_value[0, 0].Contains("X") && _value[1, 0].Contains("X") && _value[2, 0].Contains("X"))
         {
             return "Player X WIN!";
         }
-        return _value.Cast<string?>().Aggregate(string.Empty, (current, item) => current + item);
+
+        return string.Empty;
     }
 
     public void AddMotion(Token token, Position position)
