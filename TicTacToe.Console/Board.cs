26a4d4449d5e594408719e131721fef8c04f7903
cvigueras <carlosj.vigueras@gmail.com>
2023-04-18 08:56:47 +0200

    Feat: Win PlayerX By First Row

--- TicTacToe.Console/Board.cs
+++ TicTacToe.Console/Board.cs
@@ -11,6 +11,10 @@ public class Board
 
     public string Print()
     {
+        if (_value[0, 0].Contains("X") && _value[0, 1].Contains("X") && _value[0, 2].Contains("X"))
+        {
+            return "Player X WIN!";
+        }
         return _value.Cast<string?>().Aggregate(string.Empty, (current, item) => current + item);
     }
 
