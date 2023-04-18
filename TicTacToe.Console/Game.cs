158747ba9b8b966e6cf2498bd72edfe3309c4c7d
cvigueras <carlosj.vigueras@gmail.com>
2023-04-18 11:55:27 +0200

    Refactor: extracted class Game and Board methods.

---
+++ TicTacToe.Console/Game.cs
@@ -0,0 +1,37 @@
+﻿namespace TicTacToe.Console;
+
+public class Game
+{
+
+    private readonly Board Board;
+
+    public Game()
+    {
+        Board = new Board();
+    }
+
+    public string Print()
+    {
+        var winner = Board.CheckWinnerByRow();
+        if (!string.IsNullOrEmpty(winner))
+        {
+            return winner;
+        }
+        winner = Board.CheckWinnerByColumn();
+        if (!string.IsNullOrEmpty(winner))
+        {
+            return winner;
+        }
+        winner = Board.CheckWinnerByDiagonal();
+        if (!string.IsNullOrEmpty(winner))
+        {
+            return winner;
+        }
+        return Board.Value.Cast<string?>().Aggregate(string.Empty, (current, item) => current + item);
+    }
+
+    public void AddMotion(Token token, Position position)
+    {
+        Board.Value[position.X, position.Y] = $"[{token}]";
+    }
+}
