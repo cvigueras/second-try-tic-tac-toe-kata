9c4d58ba7059ff09dc9edd970c0e7dd3d845b274
cvigueras <carlosj.vigueras@gmail.com>
2023-04-19 10:34:50 +0200

    Feat: Draw Match When No body Win

--- TicTacToe.Console/Game.cs
+++ TicTacToe.Console/Game.cs
@@ -4,11 +4,15 @@ public class Game
 {
     private readonly Board _board;
     private Token _tokenTurn;
+    private int _numberOfPlays;
+    private readonly int _maxNumberOfPlays;
 
     public Game()
     {
         _board = new Board();
         _tokenTurn = Token.X;
+        _maxNumberOfPlays = 9;
+        _numberOfPlays = 0;
     }
 
     public string Print()
@@ -23,16 +27,35 @@ public class Game
 
     private string CheckWinner()
     {
-        return !string.IsNullOrEmpty(_board.GetWinnerByRow()) ? _board.GetWinnerByRow() :
-            !string.IsNullOrEmpty(_board.GetWinnerByColumn()) ? _board.GetWinnerByColumn() :
-            !string.IsNullOrEmpty(_board.CheckWinnerByDiagonal()) ? _board.CheckWinnerByDiagonal() : string.Empty;
+        if (!string.IsNullOrEmpty(_board.GetWinnerByRow()))
+        {
+            return _board.GetWinnerByRow();
+        }
+
+        if (!string.IsNullOrEmpty(_board.GetWinnerByColumn()))
+        {
+            return _board.GetWinnerByColumn();
+        }
+
+        if (!string.IsNullOrEmpty(_board.CheckWinnerByDiagonal()))
+        {
+            return _board.CheckWinnerByDiagonal();
+        }
+
+        if (_numberOfPlays == _maxNumberOfPlays)
+        {
+            return "Draw!";
+        }
+
+        return string.Empty;
+
     }
 
     public void AddMotion(Position position)
     {
         IsValidMovement(position);
         _board.Value[position.X, position.Y] = $"[{_tokenTurn}]";
-        ChangeTurn();
+        SetTurn();
     }
 
     private void IsValidMovement(Position position)
@@ -43,8 +66,9 @@ public class Game
         }
     }
 
-    private void ChangeTurn()
+    private void SetTurn()
     {
+        _numberOfPlays++;
         switch (_tokenTurn)
         {
             case Token.X:
